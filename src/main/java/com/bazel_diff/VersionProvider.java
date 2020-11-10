4c58a88fffe797b8185b63383108818f5d66b450
Maxwell Elliott <maxwell.elliott@gotinder.com>
2020-11-10 11:03:37 -0500

    Propogate error code
    Fixes #23

--- src/main/java/com/bazel_diff/VersionProvider.java
+++ src/main/java/com/bazel_diff/VersionProvider.java
@@ -5,7 +5,7 @@ import picocli.CommandLine.IVersionProvider;
 class VersionProvider implements IVersionProvider {
     public String[] getVersion() throws Exception {
         return new String[] {
-            "1.1.1"
+            "1.2.0"
         };
     }
 }

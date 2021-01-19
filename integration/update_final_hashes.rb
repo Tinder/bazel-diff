e6aae633b77b526f6024ed4a7257c8a709b20dfb
Patrick Balestra <me@patrickbalestra.com>
2021-01-19 14:00:42 +0100

    Add rules_docker integration test

--- integration/update_final_hashes.rb
+++
@@ -1,2 +0,0 @@
-content = File.read("/tmp/final_hashes_json.json").gsub(/:StringGenerator.java": \"\w+\"/, ':StringGenerator.java": "modifiedhash"')
-File.write("/tmp/final_hashes_json.json", content)

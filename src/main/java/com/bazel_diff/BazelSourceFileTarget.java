5636270d21af0c7662030b3d75b6636bd8b4f59e
eric wang <eric@canva.com>
2022-04-07 11:17:14 +1000

    fix path

--- src/main/java/com/bazel_diff/BazelSourceFileTarget.java
+++ src/main/java/com/bazel_diff/BazelSourceFileTarget.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -23,8 +24,8 @@ class BazelSourceFileTargetImpl implements BazelSourceFileTarget {
         if (workingDirectory != null && name.startsWith("//")) {
             String filenameSubstring = name.substring(2);
             String filenamePath = filenameSubstring.replaceFirst(":", "/");
-            String absoluteFilePath = workingDirectory.toString() + filenamePath;
-            try (RandomAccessFile sourceFile = new RandomAccessFile(absoluteFilePath, "r")) {
+            Path absoluteFilePath = Paths.get(workingDirectory.toString(), filenamePath);
+            try (RandomAccessFile sourceFile = new RandomAccessFile(absoluteFilePath.toString(), "r")) {
                 FileChannel inChannel = sourceFile.getChannel();
                 long fileSize = inChannel.size();
                 ByteBuffer buffer = ByteBuffer.allocate((int) fileSize);
@@ -35,7 +36,7 @@ class BazelSourceFileTargetImpl implements BazelSourceFileTarget {
                 finalDigest.update(buffer);
                 finalDigest.update(digest);
             } catch (FileNotFoundException e) {}
-        } else if (digest != null ){
+        } else if (digest != null ) {
             finalDigest.update(digest);
         }
         finalDigest.update(name.getBytes());

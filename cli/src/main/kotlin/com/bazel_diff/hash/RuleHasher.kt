cc343ce42e6db620857a0664011b08da77677fe0
Maxwell Elliott <56700854+tinder-maxwellelliott@users.noreply.github.com>
2022-09-26 14:12:47 -0400

    Revert "fix: pass on target depends itself (#138)" (#150)
    This reverts commit 68e99f10cefa1a0c5b0f355f1e60e1bf3ccc682a.

--- cli/src/main/kotlin/com/bazel_diff/hash/RuleHasher.kt
+++ cli/src/main/kotlin/com/bazel_diff/hash/RuleHasher.kt
@@ -50,7 +50,7 @@ class RuleHasher : KoinComponent {
                     inputRule == null && sourceDigests.containsKey(ruleInput) -> {
                         safePutBytes(sourceDigests[ruleInput])
                     }
-                    inputRule?.name != null -> {
+                    inputRule?.name != null && inputRule.name != rule.name -> {
                         val ruleInputHash = digest(
                             inputRule,
                             allRulesMap,

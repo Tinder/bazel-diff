af4e7cab96e433a590c6af10ac7615b397d96e5b
Maxwell Elliott <maxwell.elliott@gotinder.com>
2023-01-10 09:46:51 -0500

    more updates

--- bazel-diff-example.sh
+++ bazel-diff-example.sh
@@ -12,7 +12,6 @@ final_revision=$4
 starting_hashes_json="/tmp/starting_hashes.json"
 final_hashes_json="/tmp/final_hashes.json"
 impacted_targets_path="/tmp/impacted_targets.txt"
-impacted_test_targets_path="/tmp/impacted_test_targets.txt"
 bazel_diff="/tmp/bazel_diff"
 
 shared_flags=""
@@ -20,17 +19,17 @@ shared_flags=""
 # Uncomment the line below to see debug information
 # shared_flags="--config=verbose"
 
-$bazel_path run :bazel-diff $shared_flags --script_path="$bazel_diff"
+"$bazel_path" run :bazel-diff "$shared_flags" --script_path="$bazel_diff"
 
-git -C $workspace_path checkout $previous_revision --quiet
+git -C "$workspace_path" checkout "$previous_revision" --quiet
 
 echo "Generating Hashes for Revision '$previous_revision'"
-$bazel_diff generate-hashes -w $workspace_path -b $bazel_path $starting_hashes_json
+$bazel_diff generate-hashes -w "$workspace_path" -b "$bazel_path" $starting_hashes_json
 
-git -C $workspace_path checkout $final_revision --quiet
+git -C "$workspace_path" checkout $final_revision --quiet
 
 echo "Generating Hashes for Revision '$final_revision'"
-$bazel_diff generate-hashes -w $workspace_path -b $bazel_path $final_hashes_json
+$bazel_diff generate-hashes -w "$workspace_path" -b "$bazel_path" $final_hashes_json
 
 echo "Determining Impacted Targets"
 $bazel_diff get-impacted-targets -sh $starting_hashes_json -fh $final_hashes_json -o $impacted_targets_path
@@ -38,5 +37,5 @@ $bazel_diff get-impacted-targets -sh $starting_hashes_json -fh $final_hashes_jso
 IFS=$'\n' read -d '' -r -a impacted_targets < $impacted_targets_path
 formatted_impacted_targets=$(IFS=$'\n'; echo "${impacted_targets[*]}")
 echo "Impacted Targets between $previous_revision and $final_revision:"
-echo $formatted_impacted_targets
+echo "$formatted_impacted_targets"
 echo ""

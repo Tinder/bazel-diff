3f3d99e2a59a7d5052069385a740584db4398eb6
Maxwell Elliott <56700854+tinder-maxwellelliott@users.noreply.github.com>
2024-10-22 21:36:42 -0400

    Add BuildBuddy Support (#248)
    * Add BuildBuddy Support
    
    Adds BuildBuddy caching and BES support
    
    * more updates
    
    * workspace status
    
    * more updates
    
    * more flags
    
    * flags

---
+++ workspace_status.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This script will be run bazel when building process starts to
+# generate key-value information that represents the status of the
+# workspace. The output should be like
+#
+# KEY1 VALUE1
+# KEY2 VALUE2
+#
+# If the script exits with non-zero code, it's considered as a failure
+# and the output will be discarded.
+
+set -eo pipefail # exit immediately if any command fails.
+
+function remove_url_credentials() {
+  which perl >/dev/null && perl -pe 's#//.*?:.*?@#//#' || cat
+}
+
+repo_url=$(git config --get remote.origin.url | remove_url_credentials)
+echo "REPO_URL $repo_url"
+
+commit_sha=$(git rev-parse HEAD)
+echo "COMMIT_SHA $commit_sha"
+
+git_branch=$(git rev-parse --abbrev-ref HEAD)
+echo "GIT_BRANCH $git_branch"
+
+git_tree_status=$(git diff-index --quiet HEAD -- && echo 'Clean' || echo 'Modified')
+echo "GIT_TREE_STATUS $git_tree_status"

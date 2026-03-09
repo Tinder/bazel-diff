caa00cb15c8bacb45f1649c30c41f331f5f1cc27
Maxwell Elliott <56700854+tinder-maxwellelliott@users.noreply.github.com>
2026-03-09 15:19:49 -0400

    Use bazel contrib release rules (#319)
    Use upstream release rules for bcr usage

---
+++ .github/workflows/release_prep.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+# Argument provided by reusable workflow caller, see
+# https://github.com/bazel-contrib/.github/blob/master/.github/workflows/release_ruleset.yaml
+TAG=$1
+
+mkdir -p archives
+tar --exclude-vcs \
+  --exclude=bazel-* \
+  --exclude=.github \
+  --exclude=archives \
+  --exclude=cli/src/test \
+  -zcf "archives/release.tar.gz" .
+
+SHA=$(shasum -a 256 archives/release.tar.gz | awk '{print $1}')
+
+cat << EOF
+## Using Bzlmod (MODULE.bazel)
+
+Add to your \`MODULE.bazel\` file:
+
+\`\`\`starlark
+bazel_dep(name = "bazel-diff", version = "${TAG#v}")
+\`\`\`
+
+## Using WORKSPACE
+
+\`\`\`starlark
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+http_archive(
+  name = "bazel-diff",
+  sha256 = "${SHA}",
+  strip_prefix = "",
+  url = "https://github.com/Tinder/bazel-diff/releases/download/${TAG}/release.tar.gz",
+)
+\`\`\`
+EOF
